e03b7e2877400047b2ee7b1bdf4f00cf7441a1c2
BBX-XU <2606816603@qq.com>
2026-08-28 10:58:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
   build: {
     outDir: 'dist/client',
     sourcemap: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'client/index.html'),
+    },
   },
   server: {
     port: 5173,
